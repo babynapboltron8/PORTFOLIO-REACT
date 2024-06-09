2b7bf0d596c8c73bdf111c3184f54f09a9ac1b71
Baby Nap Boltron <147931096+babynapboltron8@users.noreply.github.com>
2024-06-09 09:55:53 +0800

    .

--- components/Archive.tsx
+++ components/Archive.tsx
@@ -9,7 +9,7 @@ const Archive = () => {
     <div className="max-w-contentContainer mx-auto px-4 py-24">
       <div className="w-full flex flex-col items-center">
         <h2 className="text-3xl font-titleFont font-semibold">
-          Other Noteworthy Projects
+          Small-Scale Projects
         </h2>
         <p className="text-sm font-titleFont text-textGreen">
           view the archive
