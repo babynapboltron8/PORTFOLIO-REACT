21b9b4a60564f5bf5e9d520592bb1d6e2fdf6921
Baby Nap Boltron <147931096+babynapboltron8@users.noreply.github.com>
2024-06-10 20:49:10 +0800

    .

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -9,11 +9,9 @@ const Contact = () => {
       <p className="font-titleFont text-lg text-textGreen font-semibold flex items-center tracking-wide">
       &#x2163;. What’s Next?
       </p>
-      <h2 className="font-titleFont text-5xl font-semibold">Get In Touch</h2>
+      <h2 className="font-titleFont text-5xl font-semibold">Connect With Me</h2>
       <p className="max-w-[600px] text-center text-textDark">
-        Although I’m not currently looking for any new opportunities, my inbox
-        is always open. Whether you have a question or just want to say hi, I’ll
-        try my best to get back to you!
+      Actively seeking new opportunities, I am available for work and open to discussing potential collaborations. Whether you have inquiries or simply want to connect, feel free to reach out, and I'll be glad to explore any possibilities together!
       </p>
 
       <a href="mailto:babynap8@gmail.com">
